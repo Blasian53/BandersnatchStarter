5256a2ea1de27f8f7ef0b57226c7555811663da4
rey <retkins53@gmail.com>
2023-07-16 16:57:03 -0400

    IT WORKS!!

--- app/data.py
+++ app/data.py
@@ -11,42 +11,46 @@ from certifi import where
 class Database:
     # Loads environmental variables
     load_dotenv()
-
-    # Creates class variable to keep track of all monster entries
-    monsters = []
+    database = MongoClient(getenv("DB_URL"), tlsCAFile=where())["Labs"]
 
     # Creates collection
-    def __init__(self, uri=getenv("DB_URL")):
-        self.database = MongoClient(uri, tlsCAFile=where())["Labs"]
+    def __init__(self, collection: str):
+        self.collection = self.database[collection]
 
     # Gathers specified number of documents from the Monster library
     def seed(self, amount):
+        # Creates class variable to keep track of all monster entries
+        monsters = []
         for i in range(0, amount):
-            self.monsters.append(Monster().to_dict())
-        return self.monsters
+            monsters.append(Monster().to_dict())
+        self.collection.insert_many(monsters)
 
     # Resets the entire database
     def reset(self):
-        self.monsters = []
+        self.collection.delete_many({})
 
     # Returns total number of documents in database
     def count(self) -> int:
-        return len(self.monsters)
+        return self.collection.count_documents({})
 
     # Creates a dataframe out of entire database
     def dataframe(self) -> DataFrame:
-        df = DataFrame(self.monsters)
+        df = DataFrame(self.collection.find({},{"_id":False}))
         return df
 
     # Creates a html table from dataframe, if empty then None
     def html_table(self) -> str:
-        df = DataFrame(self.monsters)
+        df = self.dataframe()
         if df.empty:
             return None
         else:
             return df.to_html()
 
 
+if __name__ == '__main__':
+
 # Calls Database and fills it with 1500 random monsters
-db = Database()
-db.seed(amount=1500)
+    db = Database("Monsters")
+    db.reset()
+    db.seed(amount=1500)
+    print(db.count())
