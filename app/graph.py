f1b6eb27713aa4b7bd9df9dab068412a8a148f43
rey <retkins53@gmail.com>
2023-07-21 20:34:23 -0400

    IT WORKS!!

--- app/graph.py
+++ app/graph.py
@@ -1,5 +1,17 @@
-from altair import Chart
+from altair import Chart, Tooltip
 
 
 def chart(df, x, y, target) -> Chart:
-    pass
+    graph = Chart(
+        data=df,
+        title=f"{y} by {x} for {target}",
+        background='#686665',
+        height=500,
+        width=500,
+    ).configure_title(fontSize=24).mark_circle(size=100).encode(
+        x=x,
+        y=y,
+        color=target,
+        tooltip=Tooltip(df.columns.to_list())
+    )
+    return graph
