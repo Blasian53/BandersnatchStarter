afc6cb5632fd88d8ff967e46da2b268ac53cb625
rey <retkins53@gmail.com>
2023-08-05 18:02:20 -0400

    first commit

--- app/machine.py
+++ app/machine.py
@@ -1,22 +1,25 @@
 # Import Libraries
-import pandas as pd
-import numpy as np
 from data import Database
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 import joblib
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder
 
 class Machine:
 # Create Model and fit it
     def __init__(self, df):
         self.df = df
 
+        encoder = LabelEncoder()
+
 # Creates x and y
-        target = df["Rarity"]
-        features = df.drop(columns="Rarity")
+        self.target = encoder.fit_transform(df["Rarity"])
+        self.features = df.drop(columns="Rarity")
+        print("Target unique values after label encoding:", set(self.target))
+        print("Features information:")
+        print(self.features.info())
 
 # Creates 3 different models
         self.RF_model = RandomForestClassifier()
@@ -24,13 +27,15 @@ class Machine:
         self.KNN_model = KNeighborsClassifier()
 
 # Trains All The Models
-        self.RF_model.fit(features, target)
-        self.LR_model.fit(features, target)
-        self.KNN_model.fit(features, target)
+        self.RF_model.fit(self.features, self.target)
+        self.LR_model.fit(self.features, self.target)
+        self.KNN_model.fit(self.features, self.target)
 
 # Gets the prediction for each model
     def __call__(self, feature_basis):
-        prediction, *_ = self.model.predict(feature_basis)
+        self.RF_pred = self.RF_model.predict(feature_basis)
+        self.LR_pred = self.LR_model.predict(feature_basis)
+        self.KNN_pred = self.KNN_model.predict(feature_basis)
 
 # Saves each model to respective filepath
     def save(self, filepath):
@@ -43,6 +48,15 @@ class Machine:
     def open(filepath):
         joblib.load(filepath)
 
-# Displays info on
+# Displays info on each model
     def info(self):
-        pass
+        RF_score = accuracy_score(self.target, self.RF_pred)
+        LR_score = accuracy_score(self.target, self.LR_pred)
+        KNN_score = accuracy_score(self.target, self.KNN_pred)
+        print(RF_score)
+        print(LR_score)
+        print(KNN_score)
+
+db = Database("Monsters").dataframe()
+test = Machine(db)
+test.info()
